77cae86a4de37a1c80c50b9a4cf91d570eb961b0
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2019-02-08 14:53:36 -0600

    material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { UploadForm } from './components/UploadForm.js'
 
 import './App.css';
@@ -11,21 +17,43 @@ class App extends Component {
     // console.log(results)
   }
   _renderListErrors () {
-    const { results } = this.state
-    return results.map((swaggerError, i) => {
-      return <p key={i}>{swaggerError.message}</p>
-    })
+    const { results } = this.state;
+    // return results.map((swaggerError, i) => {
+    //   return <p key={i}>{swaggerError.message}</p>
+    // })
+
+    return (
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Ruta</TableCell>
+                <TableCell>Error</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {results.map((row, i) => (
+                <TableRow key={i}>
+                  <TableCell>{row.dataPath}</TableCell>
+                  <TableCell>{row.message}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
   }
 
   _renderFirst= () => {
     return this.state.results === true
-      ? <p> VALIDO </p>
+      ? <p> El archivo cargado se ha comprobado satisfactoriamente </p>
       : this._renderListErrors()
   }
   render() {
     return (
         <div className='App'>
           <UploadForm onResults={this._handleResults}/>
+          <br/>
           {this.state.usedForm
             ? this._renderFirst()
             : <small> Debes cargar un archivo </small>
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,91 +1,122 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 let Ajv = require('ajv');
 let SwaggerParser = require('swagger-parser');
 
 export class UploadForm extends Component {
   state = {
     file: '',
+    disabled: false,
+    label: 'Cargar Archivo'
 }
 
-  handleSubmit = (e) => {
-      e.preventDefault();
-      let upload = this.state.file;
-      // fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
-      //   .then(
-      //     function(response) {
-      //       if (response.status !== 200) {
-      //         console.log('Error al obtener la especificación: ' +
-      //           response.status);
-      //         return;
-      //       }
-      //       response.json().then(function(data) {
-      //         SwaggerParser.validate(data, function(err, esquema) {
-      //           if (err) {
-      //             console.error(err);
-      //           }
-      //           else {
-      //             let ajv = new Ajv({ allErrors: true });
-      //             let validate = ajv.compile(esquema.components.schemas.Declaraciones);
-      //             let valid = validate(JSON.parse(upload));
-      //             if (valid) {
-      //                 console.log('Valid!');
-      //                 this.props.onResults(valid)
-      //             } else {
-      //                 console.log('Invalid: ' + ajv.errorsText(validate.errors));
-      //                 this.props.onResults(ajv.errorsText(validate.errors))
-      //             }
-      //           }
-      //         });
-      //       });
-      //     }
-      //   )
-      //   .catch(function(err) {
-      //     console.log('Fetch Error :-S', err);
-      //   });
-      fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
-        .then(res => res.json())
-        .then(results => {
-          // console.log(results)
-          SwaggerParser.validate(results)
-            .then((esquema) => {
-              // this.props.onResults(api)
-              let ajv = new Ajv({ allErrors: true });
-              let validate = ajv.compile(esquema.components.schemas.Declaraciones);
-              let valid = validate(JSON.parse(upload));
-              if (valid) {
-                  // console.log('Valid!');
-                  this.props.onResults(valid)
-              } else {
-                  // console.log('Invalid: ' + ajv.errorsText(validate.errors));
-                  this.props.onResults(validate.errors)
-              }
-            })
-          .catch(function(err) {
-            console.error(err);
-          });
-      })
-    }
+  // handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     let upload = this.state.file;
+  //     // fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
+  //     //   .then(
+  //     //     function(response) {
+  //     //       if (response.status !== 200) {
+  //     //         console.log('Error al obtener la especificación: ' +
+  //     //           response.status);
+  //     //         return;
+  //     //       }
+  //     //       response.json().then(function(data) {
+  //     //         SwaggerParser.validate(data, function(err, esquema) {
+  //     //           if (err) {
+  //     //             console.error(err);
+  //     //           }
+  //     //           else {
+  //     //             let ajv = new Ajv({ allErrors: true });
+  //     //             let validate = ajv.compile(esquema.components.schemas.Declaraciones);
+  //     //             let valid = validate(JSON.parse(upload));
+  //     //             if (valid) {
+  //     //                 console.log('Valid!');
+  //     //                 this.props.onResults(valid)
+  //     //             } else {
+  //     //                 console.log('Invalid: ' + ajv.errorsText(validate.errors));
+  //     //                 this.props.onResults(ajv.errorsText(validate.errors))
+  //     //             }
+  //     //           }
+  //     //         });
+  //     //       });
+  //     //     }
+  //     //   )
+  //     //   .catch(function(err) {
+  //     //     console.log('Fetch Error :-S', err);
+  //     //   });
+  //     fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
+  //       .then(res => res.json())
+  //       .then(results => {
+  //         // console.log(results)
+  //         SwaggerParser.validate(results)
+  //           .then((esquema) => {
+  //             // this.props.onResults(api)
+  //             let ajv = new Ajv({ allErrors: true });
+  //             let validate = ajv.compile(esquema.components.schemas.Declaraciones);
+  //             let valid = validate(JSON.parse(upload));
+  //             if (valid) {
+  //                 // console.log('Valid!');
+  //                 this.props.onResults(valid)
+  //             } else {
+  //                 // console.log('Invalid: ' + ajv.errorsText(validate.errors));
+  //                 this.props.onResults(validate.errors)
+  //             }
+  //           })
+  //         .catch(function(err) {
+  //           console.error(err);
+  //         });
+  //     })
+  //   }
 
 // Handle Upload Using FileReader
 handleFile = (e) => {
     let reader = new FileReader();
     let file = e.target.files[0];
+    let upload;
     reader.onloadend = (event) => {
         // console.log(event.target.result);
-        this.setState({
-            file: event.target.result
-        });
+        this.setState({ file: event.target.result });
+        upload = event.target.result;
+        this.setState({disabled: !this.state.disabled, label: 'Validando'});
     }
     reader.readAsText(file)
+
+    fetch('https://raw.githubusercontent.com/PDNMX/api_docs/master/S1/oas/declaraciones.json')
+      .then(res => res.json())
+      .then(results => {
+        // console.log(results)
+        SwaggerParser.validate(results)
+          .then((esquema) => {
+            // this.props.onResults(api)
+            let ajv = new Ajv({ allErrors: true });
+            let validate = ajv.compile(esquema.components.schemas.Declaraciones);
+            let valid = validate(JSON.parse(upload));
+            this.setState({disabled: !this.state.disabled, label: 'Terminado'});
+            if (valid) {
+                // console.log('Valid!');
+                this.props.onResults(valid)
+            } else {
+                // console.log('Invalid: ' + ajv.errorsText(validate.errors));
+                console.log(validate.errors)
+                this.props.onResults(validate.errors)
+            }
+          })
+        .catch(function(err) {
+          console.error(err);
+        });
+    })
 }
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit}>
-              <input type="file" onChange={this.handleFile}/>
-              <button>Submit</button>
-        </form>
+      <div>
+        <Button variant="contained" component="label" disabled={this.state.disabled}>
+          {this.state.label}
+          <input onChange={this.handleFile} type="file" style={{ display: "none" }}
+          />
+        </Button>
+      </div>
     );
   }
 }
